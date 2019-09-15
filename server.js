0dc2c92c9be3e052e519bc21227a5ad464a1a34b
Daniel Kempner <djkempner@gmail.com>
2019-09-14 21:02:10 -0700

    serve build in production

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const path = require('path');
+const path = require("path");
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+app.use("/", express.static("build"));
+
 // create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
 });
