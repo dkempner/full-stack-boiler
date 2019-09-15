30b05dfafb35036214f655ba0146016143aba253
Daniel Kempner <djkempner@gmail.com>
2019-09-14 20:44:55 -0700

    material stuffs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Button } from "@material-ui/core";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import "./App.css";
 
 function App() {
+  const [bag, setBag] = React.useState([]);
+  const removeEffect = React.useEffect(() => {
+    setBag(["Initial Memes"]);
+  }, []);
+
+  const onClick = () => {
+    fetch("/express_backend")
+      .then(res => res.json())
+      .then(res => {
+        setBag([...bag, JSON.stringify(res)]);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button variant="contained" color="primary" onClick={onClick}>Make Request</Button>
+        <List>
+          {bag.map(b => {
+            return <ListItem>
+              <ListItemText>{b}</ListItemText>
+            </ListItem>;
+          })}
+        </List>
         <a
           className="App-link"
           href="https://reactjs.org"
